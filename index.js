205e819b51fa2b2e5e63a5a634c8261529550a1f
sivanatarjan <sivanatarajan.s@zohocorp.com>
2018-10-21 19:03:21 +0530

    added tests

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 var stylelint = require("stylelint");
 var ruleName = "stylelint/stylelint-no-empty-selector";
-var nearestSelector="";
+var nearestSelector = "";
 var messages = stylelint.utils.ruleMessages(ruleName, {
   expected: function(selector) {
-    nearestSelector="";
-    if(selector.length>0)
-    {
-        return "Unexpected empty selector near "+selector;
+    nearestSelector = "";
+    if (selector.length > 0) {
+      return "Unexpected empty selector near " + selector;
+    } else {
+      return "Unexpected empty selector";
     }
-
-      else{
-        return "Unexpected empty selector";
-      }
   }
 });
 module.exports = stylelint.createPlugin(ruleName, function(enabled) {
@@ -20,24 +17,28 @@ module.exports = stylelint.createPlugin(ruleName, function(enabled) {
   }
   return function(root, result) {
     root.walkRules(rule => {
-    if(rule !=undefined ){
-      rule.selectors.forEach(function(selector){
-        if(selector.trim().length==0)
-        {
-          stylelint.utils.report({
-            result,
-            ruleName,
-            message: messages.expected(nearestSelector),
-            node: rule,
-            word: rule.node
 
-          });
+
+      if (rule != undefined) {
+        var selectors = rule.selector.split(',');
+        var selLength = selectors.length;
+        for (var i = 0; i < selLength; i++) {
+          if (selectors[i].trim().length == 0) {
+            stylelint.utils.report({
+              result,
+              ruleName,
+              message: messages.expected(nearestSelector),
+              node: rule,
+              word: rule.node
+
+            });
+          } else {
+            nearestSelector = selectors[i];
+          }
         }
-        else{
-          nearestSelector=selector;
-        }
-      })
-    }
-  });
-}
+
+      }
+    });
+  }
+
 });
